ddd66675d09496646369d6cc14820992f99376ab
TianYaKe <liulong@hzins.com>
2016-05-12 14:44:41 +0800

    UIView +  截屏

---
+++ UIView+UISnapshotting/UIView+UISnapshotting/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  UIView+UISnapshotting
+//
+//  Created by long on 5/12/16.
+//  Copyright © 2016 long. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
